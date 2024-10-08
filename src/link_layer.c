94831854c8b20c775f8b05aec180e8ae0ca5eace
rebelojoao <rebelo0912joao@outlook.com>
2024-10-08 21:39:37 +0100

    making code more modular

--- src/link_layer.c
+++ src/link_layer.c
@@ -21,6 +21,167 @@ void alarmHandler(int signal)
     printf("Alarm #%d\n", alarmCount);
 }
 
+void initializeAlarm() {
+    struct sigaction act = {0};
+    act.sa_handler = &alarmHandler;
+    if (sigaction(SIGALRM, &act, NULL) == -1) {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void sendUAFrame() {
+    char buf_s[BUF_SIZE] = {0};
+
+    // UA Frame
+    buf_s[0] = 0x7E; // Flag
+    buf_s[1] = 0x03; // Address
+    buf_s[2] = 0x07; // Control
+    buf_s[3] = 0x04; // BCC
+    buf_s[4] = 0x7E; // Flag
+
+    int bytes_s = writeBytes(buf_s, BUF_SIZE);
+    printf("%d bytes written (UA Frame)\n", bytes_s);
+}
+
+void sendSETFrame() {
+    char buf_s[BUF_SIZE] = {0};
+
+    // SET Frame
+    buf_s[0] = 0x7E; // Flag
+    buf_s[1] = 0x03; // Address
+    buf_s[2] = 0x03; // Control
+    buf_s[3] = 0x00; // BCC
+    buf_s[4] = 0x7E; // Flag
+
+    int bytes_s = writeBytes(buf_s, BUF_SIZE);
+    printf("%d bytes written (SET Frame)\n", bytes_s);
+}
+
+int llOpenRxStateMachine() {
+    enum State { START, FLAG_RCV, A_RCV, C_RCV, BCC_OK, STOP_STATE };
+    enum State state = START;
+    char buf[BUF_SIZE + 1] = {0}; // +1 for the final '\0' char
+
+    while (STOP == FALSE) {
+        int bytes = readByte(buf); 
+        if (bytes > 0) {
+            switch (state) {
+            case START:
+                if (buf[0] == 0x7E) 
+                    state = FLAG_RCV;
+                break;
+            case FLAG_RCV:
+                if (buf[0] == 0x03) 
+                    state = A_RCV;
+                else if (buf[0] != 0x7E)
+                    state = START;
+                break;
+            case A_RCV:
+                if (buf[0] == 0x03) 
+                    state = C_RCV;
+                else if (buf[0] == 0x7E)
+                    state = FLAG_RCV;
+                else
+                    state = START;
+                break;
+            case C_RCV:
+                if (buf[0] == (0x03 ^ 0x03)) 
+                    state = BCC_OK;
+                else if (buf[0] == 0x7E)
+                    state = FLAG_RCV;
+                else
+                    state = START;
+                break;
+            case BCC_OK:
+                if (buf[0] == 0x7E) 
+                    state = STOP_STATE;
+                else
+                    state = START;
+                break;
+            case STOP_STATE:
+                STOP = TRUE;
+                break;
+            }
+        }
+        usleep(100000); // microsecond precision
+    }
+
+    // Sending UA Frame 
+    if (state == STOP_STATE) {
+        sendUAFrame();
+        return 1; // Success
+    }
+    return -1; // Failure
+}
+
+int llOpenTxStateMachine() {
+    enum State { START, FLAG_RCV, A_RCV, C_RCV, BCC_OK, STOP_STATE };
+    enum State state = START;
+    char buf[BUF_SIZE + 1] = {0}; // +1 for the final '\0' char
+    int alarmCount = 0;
+
+    while (STOP == FALSE && alarmCount < MAX_RETRIES) {
+        if (!alarmEnabled) {
+            sendSETFrame(); // Sending SET Frame
+            alarm(3); // Set alarm for 3 seconds
+            alarmEnabled = TRUE;
+        }
+
+        int bytes = readByte(buf);
+        if (bytes > 0) {
+            alarm(0); 
+            alarmEnabled = FALSE; // Reset the alarm status
+            alarmCount = 0; // Reset on successful read
+
+            switch (state) {
+            case START:
+                if (buf[0] == 0x7E) 
+                    state = FLAG_RCV;
+                break;
+            case FLAG_RCV:
+                if (buf[0] == 0x03) 
+                    state = A_RCV;
+                else if (buf[0] != 0x7E)
+                    state = START;
+                break;
+            case A_RCV:
+                if (buf[0] == 0x07) 
+                    state = C_RCV;
+                else if (buf[0] == 0x7E)
+                    state = FLAG_RCV;
+                else
+                    state = START;
+                break;
+            case C_RCV:
+                if (buf[0] == (0x03 ^ 0x07)) 
+                    state = BCC_OK;
+                else if (buf[0] == 0x7E)
+                    state = FLAG_RCV;
+                else
+                    state = START;
+                break;
+            case BCC_OK:
+                if (buf[0] == 0x7E) 
+                    state = STOP_STATE;
+                else
+                    state = START;
+                break;
+            case STOP_STATE:
+                STOP = TRUE;
+                break;
+            }
+        } else if (alarmEnabled == FALSE) {
+            alarmCount++; // Increment alarm count if no bytes are read
+            if (alarmCount >= MAX_RETRIES) {
+                printf("Max alarms reached. Aborting.\n");
+                return -1; 
+            }
+        }
+    }
+    return 1; // Success
+}
+
 ////////////////////////////////////////////////
 // LLOPEN
 ////////////////////////////////////////////////
@@ -32,154 +193,15 @@ int llopen(LinkLayer connectionParameters)
         return -1;
     }
 
-    struct sigaction act = {0};
-    act.sa_handler = &alarmHandler;
-    if (sigaction(SIGALRM, &act, NULL) == -1) {
-        perror("sigaction");
-        exit(EXIT_FAILURE);
+    initializeAlarm();
+
+    if (connectionParameters.role == LlRx) {
+        return llOpenRxStateMachine(); 
+    } else if (connectionParameters.role == LlTx) {
+        return llOpenTxStateMachine(); 
     }
 
-    enum State { START, FLAG_RCV, A_RCV, C_RCV, BCC_OK, STOP_STATE };
-    enum State state = START;
-    
-    char buf[BUF_SIZE + 1] = {0}; // +1 space for the final '\0' char
-    int alarmCount = 0; 
-    
-    if(connectionParameters.role == LlRx){
-            
-        while (STOP == FALSE)
-        {
-            int bytes = readByte(buf); 
-            if (bytes > 0)
-            {
-                switch (state)
-                {
-                case START:
-                    if (buf[0] == 0x7E) 
-                        state = FLAG_RCV;
-                    break;
-                case FLAG_RCV:
-                    if (buf[0] == 0x03) 
-                        state = A_RCV;
-                    else if (buf[0] != 0x7E)
-                        state = START;
-                    break;
-                case A_RCV:
-                    if (buf[0] == 0x03) 
-                        state = C_RCV;
-                    else if (buf[0] == 0x7E)
-                        state = FLAG_RCV;
-                    else
-                        state = START;
-                    break;
-                case C_RCV:
-                    if (buf[0] == (0x03 ^ 0x03)) 
-                        state = BCC_OK;
-                    else if (buf[0] == 0x7E)
-                        state = FLAG_RCV;
-                    else
-                        state = START;
-                    break;
-                case BCC_OK:
-                    if (buf[0] == 0x7E) 
-                        state = STOP_STATE;
-                    else
-                        state = START;
-                    break;
-                case STOP_STATE:
-                    STOP = TRUE;
-                    break;
-                }
-            }
-            sleep(0.1);
-        }
-
-        // Sending UA Frame 
-        if(state == STOP_STATE){
-            char buf_s[BUF_SIZE] = {0};
-
-            // UA Frame
-            buf_s[0] = 0x7E;
-            buf_s[1] = 0x03;
-            buf_s[2] = 0x07;
-            buf_s[3] = 0x04;
-            buf_s[4] = 0x7E;  
-            int bytes_s = writeBytes(buf_s,BUF_SIZE);
-            printf("%d bytes written\n",bytes_s);
-        }
-    }
-
-    if(connectionParameters.role == LlTx) {
-        while (STOP == FALSE && alarmCount < MAX_RETRIES) {
-            if (!alarmEnabled) {
-                char buf_s[BUF_SIZE] = {0};
-                // SET FRAME
-                buf_s[0] = 0x7E;
-                buf_s[1] = 0x03;
-                buf_s[2] = 0x03;
-                buf_s[3] = 0x00;
-                buf_s[4] = 0x7E;  
-
-                int bytes_s = writeBytes(buf_s, BUF_SIZE);
-                printf("%d bytes written\n", bytes_s);
-
-                alarm(3); // Set alarm for 3 seconds
-                alarmEnabled = TRUE;
-            }
-
-            int bytes = readByte(buf);
-            if (bytes > 0) {
-                alarm(0); 
-                alarmEnabled = FALSE; // Reset the alarm status
-                alarmCount = 0; // Reset the alarm count on successful read
-
-                switch (state) {
-                case START:
-                    if (buf[0] == 0x7E) 
-                        state = FLAG_RCV;
-                    break;
-                case FLAG_RCV:
-                    if (buf[0] == 0x03) 
-                        state = A_RCV;
-                    else if (buf[0] != 0x7E)
-                        state = START;
-                    break;
-                case A_RCV:
-                    if (buf[0] == 0x07) 
-                        state = C_RCV;
-                    else if (buf[0] == 0x7E)
-                        state = FLAG_RCV;
-                    else
-                        state = START;
-                    break;
-                case C_RCV:
-                    if (buf[0] == (0x03 ^ 0x07)) 
-                        state = BCC_OK;
-                    else if (buf[0] == 0x7E)
-                        state = FLAG_RCV;
-                    else
-                        state = START;
-                    break;
-                case BCC_OK:
-                    if (buf[0] == 0x7E) 
-                        state = STOP_STATE;
-                    else
-                        state = START;
-                    break;
-                case STOP_STATE:
-                    STOP = TRUE;
-                    break;
-                }
-            } else if (alarmEnabled == FALSE) {
-                alarmCount++; // Increment alarm count if no bytes are read
-                if (alarmCount >= MAX_RETRIES) {
-                    printf("Max alarms reached. Aborting.\n");
-                    return -1; 
-                }
-            }
-        }
-    }
-    return 1; // Success
+    return -1;
 }
 
 ////////////////////////////////////////////////
