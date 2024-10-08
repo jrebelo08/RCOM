6e18b97d09fcd0c2882026f7a869cdcddbadc19a
Cruddle <up202107694@up.pt>
2024-10-08 19:45:34 +0100

    Mergin

--- src/link_layer.c
+++ src/link_layer.c
@@ -2,14 +2,12 @@
 
 #include "link_layer.h"
 #include "serial_port.h"
+
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
 #define BUF_SIZE 8
-#define FALSE 0
-#define TRUE 1
 
-volatile int STOP = FALSE;
-
+volatile int STOP = false;
 ////////////////////////////////////////////////
 // LLOPEN
 ////////////////////////////////////////////////
@@ -20,13 +18,16 @@ int llopen(LinkLayer connectionParameters)
     {
         return -1;
     }
-
     enum State { START, FLAG_RCV, A_RCV, C_RCV, BCC_OK, STOP_STATE };
     enum State state = START;
+    
     unsigned char buf[BUF_SIZE + 1] = {0}; // +1 space for the final '\0' char
 
     if(connectionParameters.role == LlRx){
             // Loop for input
+
+
+
         while (STOP == FALSE)
         {
             int bytes = readByte(buf); 
@@ -78,12 +79,12 @@ int llopen(LinkLayer connectionParameters)
         if(state == STOP_STATE){
             unsigned char buf_s[BUF_SIZE] = {0};
 
+            // UA Frame
             buf_s[0] = 0x7E;
             buf_s[1] = 0x03;
             buf_s[2] = 0x07;
             buf_s[3] = 0x04;
             buf_s[4] = 0x7E;  
-
             int bytes_s = writeBytes(buf_s,BUF_SIZE);
             printf("%d bytes written\n",bytes_s);
         }
@@ -92,18 +93,17 @@ int llopen(LinkLayer connectionParameters)
     if(connectionParameters.role == LlTx){
         if(state == START){
             unsigned char buf_s[BUF_SIZE] = {0};
-            // SET FRAME
+
+            // SET Frame
             buf_s[0] = 0x7E;
             buf_s[1] = 0x03;
             buf_s[2] = 0x03;
             buf_s[3] = 0x00;
             buf_s[4] = 0x7E;  
-
             int bytes_s = writeBytes(buf_s,BUF_SIZE);
             printf("%d bytes written\n",bytes_s);
         }
-
-         while (STOP == FALSE)
+        while (STOP == FALSE)
         {
             int bytes = readByte(buf); 
             if (bytes > 0)
@@ -121,7 +121,7 @@ int llopen(LinkLayer connectionParameters)
                         state = START;
                     break;
                 case A_RCV:
-                    if (buf[0] == 0x03) 
+                    if (buf[0] == 0x07) 
                         state = C_RCV;
                     else if (buf[0] == 0x7E)
                         state = FLAG_RCV;
