0554bb46d520643abafe0e2d51f603a9d357e188
rebelojoao <rebelo0912joao@outlook.com>
2024-10-12 13:47:32 +0100

    llwrite needs to be tested

--- src/link_layer.c
+++ src/link_layer.c
@@ -10,6 +10,23 @@
 #define TRUE 1
 #define MAX_RETRIES 3
 
+// Frame Control Constants
+#define FLAG 0x7E                     // Flag byte
+#define ADDR_TX 0x03 // Address field for transmitter to receiver
+#define ADDR_RX 0x01 // Address field for receiver to transmitter
+
+// Control Field Constants
+#define CTRL_SET 0x03      // Control field for SET frame
+#define CTRL_UA 0x07       // Control field for UA frame
+#define CTRL_RR1 0x05       // Control field for RR (Receiver Ready)
+#define CTRL_REJ0 0x01      // Control field for REJ (Reject)
+#define CTRL_DISC 0x0B     // Control field for DISC (Disconnect)
+#define CTRL_I_0 0x00      // Control field for I-frame with sequence number 0
+#define CTRL_I_1 0x40      // Control field for I-frame with sequence number 1
+
+// Byte stuffing
+#define ESC 0x7d
+
 volatile int STOP = FALSE;
 int alarmEnabled = FALSE; 
 int alarmCount = 0;
@@ -35,11 +52,11 @@ void sendUAFrame() {
     char buf_s[BUF_SIZE] = {0};
 
     // UA Frame
-    buf_s[0] = 0x7E; // Flag
-    buf_s[1] = 0x03; // Address
-    buf_s[2] = 0x07; // Control
-    buf_s[3] = 0x04; // BCC
-    buf_s[4] = 0x7E; // Flag
+    buf_s[0] = FLAG; // Flag
+    buf_s[1] = ADDR_TX; // Address
+    buf_s[2] = CTRL_UA; // Control
+    buf_s[3] = ADDR_TX ^ CTRL_UA; // BCC
+    buf_s[4] = FLAG; // Flag
 
     int bytes_s = writeBytes(buf_s, BUF_SIZE);
     printf("%d bytes written (UA Frame)\n", bytes_s);
@@ -49,11 +66,11 @@ void sendSETFrame() {
     char buf_s[BUF_SIZE] = {0};
 
     // SET Frame
-    buf_s[0] = 0x7E; // Flag
-    buf_s[1] = 0x03; // Address
-    buf_s[2] = 0x03; // Control
-    buf_s[3] = 0x00; // BCC
-    buf_s[4] = 0x7E; // Flag
+    buf_s[0] = FLAG; // Flag
+    buf_s[1] = ADDR_TX; // Address
+    buf_s[2] = CTRL_SET; // Control
+    buf_s[3] = ADDR_TX ^ CTRL_SET; // BCC
+    buf_s[4] = FLAG; // Flag
 
     int bytes_s = writeBytes(buf_s, BUF_SIZE);
     printf("%d bytes written (SET Frame)\n", bytes_s);
@@ -64,38 +81,40 @@ int llOpenRxStateMachine() {
     enum State state = START;
     char buf[BUF_SIZE + 1] = {0}; // +1 for the final '\0' char
 
+    STOP = FALSE;
+
     while (STOP == FALSE) {
         int bytes = readByte(buf); 
         if (bytes > 0) {
             switch (state) {
             case START:
-                if (buf[0] == 0x7E) 
+                if (buf[0] == FLAG) 
                     state = FLAG_RCV;
                 break;
             case FLAG_RCV:
-                if (buf[0] == 0x03) 
+                if (buf[0] == ADDR_TX) 
                     state = A_RCV;
-                else if (buf[0] != 0x7E)
+                else if (buf[0] != FLAG)
                     state = START;
                 break;
             case A_RCV:
-                if (buf[0] == 0x03) 
+                if (buf[0] == CTRL_SET) 
                     state = C_RCV;
-                else if (buf[0] == 0x7E)
+                else if (buf[0] == FLAG)
                     state = FLAG_RCV;
                 else
                     state = START;
                 break;
             case C_RCV:
-                if (buf[0] == (0x03 ^ 0x03)) 
+                if (buf[0] == (ADDR_TX ^ CTRL_SET)) 
                     state = BCC_OK;
-                else if (buf[0] == 0x7E)
+                else if (buf[0] == FLAG)
                     state = FLAG_RCV;
                 else
                     state = START;
                 break;
             case BCC_OK:
-                if (buf[0] == 0x7E) 
+                if (buf[0] == FLAG) 
                     state = STOP_STATE;
                 else
                     state = START;
@@ -122,6 +141,8 @@ int llOpenTxStateMachine() {
     char buf[BUF_SIZE + 1] = {0}; // +1 for the final '\0' char
     int alarmCount = 0;
 
+    STOP = FALSE;
+
     while (STOP == FALSE && alarmCount < MAX_RETRIES) {
         if (!alarmEnabled) {
             sendSETFrame(); // Sending SET Frame
@@ -137,33 +158,33 @@ int llOpenTxStateMachine() {
 
             switch (state) {
             case START:
-                if (buf[0] == 0x7E) 
+                if (buf[0] == FLAG) 
                     state = FLAG_RCV;
                 break;
             case FLAG_RCV:
-                if (buf[0] == 0x03) 
+                if (buf[0] == ADDR_TX) 
                     state = A_RCV;
-                else if (buf[0] != 0x7E)
+                else if (buf[0] != FLAG)
                     state = START;
                 break;
             case A_RCV:
-                if (buf[0] == 0x07) 
+                if (buf[0] == CTRL_UA) 
                     state = C_RCV;
-                else if (buf[0] == 0x7E)
+                else if (buf[0] == FLAG)
                     state = FLAG_RCV;
                 else
                     state = START;
                 break;
             case C_RCV:
-                if (buf[0] == (0x03 ^ 0x07)) 
+                if (buf[0] == (ADDR_TX ^ CTRL_UA)) 
                     state = BCC_OK;
-                else if (buf[0] == 0x7E)
+                else if (buf[0] == FLAG)
                     state = FLAG_RCV;
                 else
                     state = START;
                 break;
             case BCC_OK:
-                if (buf[0] == 0x7E) 
+                if (buf[0] == FLAG) 
                     state = STOP_STATE;
                 else
                     state = START;
@@ -205,79 +226,136 @@ int llopen(LinkLayer connectionParameters)
     return -1;
 }
 
+unsigned char* buildFrame(const unsigned char *buf, int bufSize, int *frameSize) {
+    *frameSize = bufSize + 6;
+    unsigned char *frame = (unsigned char *)malloc(*frameSize);
+
+    frame[0] = FLAG; // Start flag
+    frame[1] = ADDR_TX; // Address field
+    frame[2] = (frame_number == 0) ? CTRL_I_0 : CTRL_I_1; // Control field
+    frame[3] = frame[1] ^ frame[2]; // BCC1
+
+    unsigned char bcc2 = 0;
+    for (int i = 0; i < bufSize; i++) {
+        frame[i + 4] = buf[i];
+        bcc2 ^= buf[i];
+    }
+    frame[bufSize + 4] = bcc2; // BCC2
+    frame[bufSize + 5] = FLAG; // End flag
+
+    return frame;
+}
+
+unsigned char* byteStuffing(const unsigned char *frame, int frameSize, int *stuffedSize) {
+    unsigned char *stuffedFrame = (unsigned char *)malloc(frameSize * 2); 
+    *stuffedSize = 0;
+
+    for (int i = 0; i < frameSize; i++) {
+        if (frame[i] == FLAG) {
+            stuffedFrame[(*stuffedSize)++] = ESC;
+            stuffedFrame[(*stuffedSize)++] = 0x5e; // FLAG substitution
+        } else if (frame[i] == ESC) {
+            stuffedFrame[(*stuffedSize)++] = ESC;
+            stuffedFrame[(*stuffedSize)++] = 0x5d; // ESC substitution
+        } else {
+            stuffedFrame[(*stuffedSize)++] = frame[i];
+        }
+    }
+    return stuffedFrame;
+}
+
 ////////////////////////////////////////////////
 // LLWRITE
 ////////////////////////////////////////////////
 int llwrite(const unsigned char *buf, int bufSize) {
-    // Frame construction
-    int totalFrameSize = bufSize + 6; // Data size + header
-    unsigned char *frame = (unsigned char *)malloc(totalFrameSize); // Allocate memory
+    
+    int frameSize;
+    unsigned char *frame = buildFrame(buf, bufSize, &frameSize);
 
-    // Constructing the frame
-    frame[0] = 0x7E; // Flag
-    frame[1] = 0x03; // Address
-    frame[2] = (frame_number == 0) ? 0x00 : 0x80; // Control
-    frame[3] = frame[1] ^ frame[2]; // BCC1
+    int stuffedSize;
+    unsigned char *stuffedFrame = byteStuffing(frame, frameSize, &stuffedSize);
+    free(frame); // Free original frame memory
 
-    // BCC2 calculation
-    unsigned char bcc2 = 0; 
-    for (int i = 0; i < bufSize; i++) {
-        frame[i + 4] = buf[i]; // Copy data
-        bcc2 ^= buf[i]; // Calculate BCC2
-    }
-    frame[totalFrameSize - 2] = bcc2; // Insert BCC2
-    frame[totalFrameSize - 1] = 0x7E; // End flag
-
-    initializeAlarm();
-    // Sending frame and waiting for acknowledgment
+    initializeAlarm(); // Initialize the alarm handler
     alarmCount = 0;
-    while (alarmCount < MAX_RETRIES) {
-        writeBytes(frame, totalFrameSize); // Send frame
+
+    STOP = FALSE;
+
+    while (STOP == FALSE && alarmCount < MAX_RETRIES) {
+        // Transmit the frame
+        writeBytes((const char *)stuffedFrame, stuffedSize); // Send the stuffed frame
         alarm(3); // Set alarm for 3 seconds
         alarmEnabled = TRUE;
 
         // Acknowledgment state machine
-        enum { START, FLAG_RCV, A_RCV, C_RCV, BCC_OK, STOP } state = START;
+        enum { START, FLAG_RCV, A_RCV, C_RCV, BCC_OK, STOP_STATE } state = START;
         unsigned char byte;
+        int reject = 0;
 
         while (alarmEnabled) { // Loop until alarm goes off or acknowledgment is received
-            if (readByte(&byte) > 0) {
+            if (readByte((char *) &byte) > 0) {
                 switch (state) {
                     case START:
-                        if (byte == 0x7E) state = FLAG_RCV;
+                        if (byte == FLAG) state = FLAG_RCV;
                         break;
                     case FLAG_RCV:
-                        if (byte == 0x03) state = A_RCV;
-                        else if (byte != 0x7E) state = START;
+                        if (byte == ADDR_TX) state = A_RCV;
+                        else if (byte != FLAG) state = START;
                         break;
                     case A_RCV:
-                        if (byte == 0x07) state = C_RCV;
-                        else if (byte == 0x7E) state = FLAG_RCV;
-                        else state = START;
+                        if (frame_number == 0 && byte == CTRL_RR1) {
+                            state = C_RCV;
+                            reject = 0; // Positive acknowledgment
+                        } else if (frame_number == 1 && byte == CTRL_REJ0) {
+                            state = C_RCV;
+                            reject = 1; // Negative acknowledgment (REJ)
+                        } else if (byte == FLAG) {
+                            state = FLAG_RCV;
+                        } else {
+                            state = START;
+                        }
                         break;
                     case C_RCV:
-                        if (byte == (0x03 ^ 0x07)) state = BCC_OK;
-                        else if (byte == 0x7E) state = FLAG_RCV;
-                        else state = START;
+                        if (byte == (ADDR_TX ^ ((reject == 0) ? CTRL_RR1 : CTRL_REJ0))) {
+                            state = BCC_OK;
+                        } else if (byte == FLAG) {
+                            state = FLAG_RCV;
+                        } else {
+                            state = START;
+                        }
                         break;
                     case BCC_OK:
-                        if (byte == 0x7E) {
-                            alarm(0); // Reset the alarm
-                            alarmEnabled = FALSE; // Acknowledgment received
-                            free(frame); // Free allocated memory
-                            return bufSize; // Successful write
+                        if (byte == FLAG) {
+                            if (reject == 0) { // Positive acknowledgment (RR)
+                                alarm(0); // Reset the alarm
+                                alarmEnabled = FALSE; // Acknowledgment received
+                                free(stuffedFrame); // Free allocated memory
+                                frame_number = 1 - frame_number; // Toggle frame number for next transmission
+                                state = STOP_STATE;
+                                return bufSize; // Successful write
+                            } else { // Negative acknowledgment (REJ)
+                                state = START; // Restart the loop for resending
+                            }
                         } else {
                             state = START; // Unexpected byte, reset state
                         }
                         break;
+                        case STOP_STATE:
+                            STOP = TRUE;
+                        break;
                 }
-            } else if(alarmEnabled == FALSE){
-
             }
         }
+
+        // Alarm triggered or maximum retries reached, retry transmission
+        if (alarmCount >= MAX_RETRIES) {
+            printf("Max retries reached. Transmission failed.\n");
+            break; // Exit if maximum retries are reached
+        }
     }
-    free(frame); // Free allocated memory after attempts
-    return -1; // Failed to send
+
+    free(stuffedFrame); // Free allocated memory after all attempts
+    return -1; // Failed to send after max retries
 }
 
 ////////////////////////////////////////////////
