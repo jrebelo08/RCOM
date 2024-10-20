9fabd4992051c2c17aaa62484aceecd916df20b7
Cruddle <up202107694@up.pt>
2024-10-20 19:16:54 +0100

    app layer init

--- include/application_layer.h
+++ include/application_layer.h
@@ -4,6 +4,17 @@
 #ifndef _APPLICATION_LAYER_H_
 #define _APPLICATION_LAYER_H_
 
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define C_DATA 1
+#define C_START 2
+#define C_END 3
+
 // Application layer main function.
 // Arguments:
 //   serialPort: Serial port name (e.g., /dev/ttyS0).
--- src/application_layer.c
+++ src/application_layer.c
@@ -2,8 +2,47 @@
 
 #include "application_layer.h"
 
+
+
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
-    // TODO
+    if (strcmp(role, "tx") == 0) {
+    // Open the file for reading
+    // Prepare the control packet for file size
+    // Establish a link layer connection
+    // Transmit the control packet
+    // Read and transmit file data in chunks
+    // Transmit the end control packet
+    // Close the connection
+    int fd = open(filename, O_RDONLY);
+        if (fd == -1) {
+            printf("Error opening \"%s\".\n", filename);
+            return;
+        }
+
+        struct stat st;
+        stat(filename, &st);
+
+
+        unsigned char *control_packet = (unsigned char*) malloc (11); // 11?
+        control_packet[0] = C_START;
+        control_packet[1] = 0;
+        control_packet[2] = 8;
+
+          unsigned long size_aux = (unsigned long) st.st_size;   
+        for (int i = 7; i >= 0; i--) {
+            control_packet[i + 3] = (unsigned char) (0xff & size_aux);
+            size_aux >>= 8;
+        }
+
+    }
+    else if (strcmp(role, "rx") == 0) {
+        // Open the file for writing
+        // Establish a link layer connection
+        // Receive the control packet to get the file size
+        // Receive and write data packets until the end control packet is received
+        // Close the connection
+    }
+
 }
